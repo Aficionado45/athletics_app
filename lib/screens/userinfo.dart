46b94739fa5477234402f9edb571a68e5c6999d4
uditverma21 <iamuv21@gmail.com>
2021-05-23 19:40:54 +0530

    UI commit

--- lib/screens/userinfo.dart
+++ lib/screens/userinfo.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: MemberInfo(),
-  ));
-}
+
 
 class MemberInfo extends StatefulWidget {
   @override
