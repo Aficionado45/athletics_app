74cffd5d3f08378a32b93499d2ba8e73a14ead3a
Prakhar <psethiya28@gmail.com>
2021-05-24 12:35:44 +0530

    ui screens

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -1,113 +1,77 @@
 import 'package:flutter/material.dart';
 
+
+
+
 class Leaderboard extends StatefulWidget {
   @override
   _LeaderboardState createState() => _LeaderboardState();
 }
 
 class _LeaderboardState extends State<Leaderboard> {
-  int _currentindex = 0;
+  int _currentindex=0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xFF143B40),
-          title: Text(
-            'Leaderboard',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-            ),
-          ),
-          centerTitle: true,
-          leading: FlatButton(
-            onPressed: () {
-              Navigator.pushNamed(context, 'homescreen');
-            },
-            child: Icon(
-              Icons.arrow_back_sharp,
-              size: 30,
-              color: Colors.white,
-            ),
-          ),
-          actions: [
-            IconButton(
-              onPressed: (){
-                Navigator.pushNamed(context, 'user');
-              },
-              icon: Icon(
-
-                Icons.account_circle,
-                color: Colors.white,
-                size: 30,
+          appBar: AppBar(
+            backgroundColor: Color(0xFF143B40),
+            title: Text("Leaderboard",
+              style: TextStyle(
+                color:Colors.white,
               ),
             ),
-          ],
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/athlete.jpeg"), fit: BoxFit.fill),
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          currentIndex: 0,
-          items: [
-            BottomNavigationBarItem(
-              icon: Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, 'homescreen');
-                  },
-                  icon: Icon(Icons.home),
+            centerTitle: true,
+            // ignore: deprecated_member_use
+            leading: FlatButton(
+              onPressed: () { /* Write listener code here */ },
+              child: Icon(
+                Icons.arrow_back_ios_rounded,
+                size: 36,
+                color: Colors.white,// add custom icons also
+              ),
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.account_circle_rounded,
+                  size: 36,
                   color: Colors.white,
-                  iconSize: 30,
+
                 ),
               ),
-
-              backgroundColor: Color(0xFF143B40),
-              // ignore: deprecated_member_use
-              title: Text(''),
-            ),
-            BottomNavigationBarItem(
-              icon: IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'leaderboard');
-                },
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: _currentindex,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                backgroundColor: Color(0xFF143B40),
+                // ignore: deprecated_member_use
+                title:Text(''),
+              ),
+              BottomNavigationBarItem(
                 icon: Icon(Icons.leaderboard),
-                color: Colors.white,
-                iconSize: 30,
+                // ignore: deprecated_member_use
+                title:Text('home'),
               ),
-              // ignore: deprecated_member_use
-              title: Text(''),
-            ),
-            BottomNavigationBarItem(
-              icon: IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'achievement');
-                },
+              BottomNavigationBarItem(
                 icon: Icon(Icons.emoji_events_rounded),
-                color: Colors.white,
-                iconSize: 30,
+
+                // ignore: deprecated_member_use
+                title:Text('home'),
               ),
-
-
-              // ignore: deprecated_member_use
-              title: Text(''),
-            ),
-            BottomNavigationBarItem(
-              icon: IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'memberlist');
-                },
-                icon: Icon(Icons.group_rounded),
-                color: Colors.white,
-                iconSize: 30,
+              BottomNavigationBarItem(
+                icon: Icon(Icons.people),
+                // ignore: deprecated_member_use
+                title:Text(''),
+              ),
+            ],
 
           ),
           body: Container(
@@ -115,14 +79,12 @@ class _LeaderboardState extends State<Leaderboard> {
               image: DecorationImage(
                 image: AssetImage("assets/background.jpeg"),
                 fit: BoxFit.cover,
-
               ),
-              // ignore: deprecated_member_use
-              title: Text(''),
             ),
-          ],
-        ),
-      ),
+
+          )
+
+      ) ,
     );
   }
 }
