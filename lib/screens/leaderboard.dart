ddeaede63e5985daed2c8fa6a7bdc48d9e26f310
Prakhar <psethiya28@gmail.com>
2021-05-25 23:13:13 +0530

    ui screens

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -44,62 +44,70 @@ class _LeaderboardState extends State<Leaderboard> {
               ),
             ],
           ),
-          bottomNavigationBar:
-          Theme(
-            data: Theme.of(context).copyWith(
-                canvasColor: Color.fromRGBO(255, 255, 255, 255),
-                primaryColor: Colors.red,
-                textTheme: Theme.of(context).textTheme.copyWith(
-                    caption: TextStyle(
-                        color: Colors
-                            .black))),
-            child: Container(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0,0,0,0),
-                child: BottomNavigationBar(
-                  showSelectedLabels: false,
-                  showUnselectedLabels: false,
-                  currentIndex: 0,
-                  items: [
-                    BottomNavigationBarItem(
+          bottomNavigationBar: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: 0,
+            items: [
+              BottomNavigationBarItem(
+                icon: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'homescreen');
+                    },
+                    icon: Icon(Icons.home),
+                    color: Colors.white,
+                    iconSize: 30,
 
-                      icon:
-                      Icon(Icons.home_rounded,
-                        size: 30,
-                      ),
-
-                      backgroundColor: Color(0xFF143B40),
-                      // ignore: deprecated_member_use
-                      title:Text(''),
-
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.leaderboard,
-                        size: 30,
-                      ),
-                      // ignore: deprecated_member_use
-                      title:Text(''),
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.emoji_events_rounded,
-                        size: 30,
-                      ),
-
-                      // ignore: deprecated_member_use
-                      title:Text(''),
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.people,
-                        size: 30,
-                      ),
-                      // ignore: deprecated_member_use
-                      title:Text(''),
-                    ),
-                  ],
+                  ),
                 ),
 
+                backgroundColor: Color(0xFF143B40),
+                // ignore: deprecated_member_use
+                title: Text(''),
               ),
-            ),
+              BottomNavigationBarItem(
+                icon: IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'leaderboard');
+                  },
+                  icon: Icon(Icons.leaderboard),
+                  color: Colors.white,
+                  iconSize: 30,
+
+                ),
+                // ignore: deprecated_member_use
+                title: Text(''),
+              ),
+              BottomNavigationBarItem(
+                icon: IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'achievement');
+                  },
+                  icon: Icon(Icons.emoji_events_rounded),
+                  color: Colors.white,
+                  iconSize: 30,
+
+                ),
+
+                // ignore: deprecated_member_use
+                title: Text(''),
+              ),
+              BottomNavigationBarItem(
+                icon: IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'memberlist');
+                  },
+                  icon: Icon(Icons.group_rounded),
+                  color: Colors.white,
+                  iconSize: 30,
+
+                ),
+                // ignore: deprecated_member_use
+                title: Text(''),
+              ),
+            ],
           ),
           body: Container(
             decoration: BoxDecoration(
