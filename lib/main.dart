9f82ee1712e2a5008f5f125157b15ae802da8120
uditverma21 <iamuv21@gmail.com>
2021-05-28 16:42:34 +0530

    UI commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:athletics_app/authentication_service.dart';
 import 'package:athletics_app/screens/Alumni.dart';
 import 'package:athletics_app/screens/members.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'screens/records.dart';
 import 'screens/welcome.dart';
 import 'screens/loginscreeen.dart';
@@ -15,8 +12,7 @@ import 'screens/achievements.dart';
 import 'screens/homescreen.dart';
 import 'screens/members.dart';
 import 'screens/Alumni.dart';
-
-Future<void> main() async {
+Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
@@ -31,47 +27,23 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      Provider<AuthenticationService>(
-        create: (_) => AuthenticationService(FirebaseAuth.instance),
-      ),
-      StreamProvider(
-        create: (context) =>context.read<AuthenticationService>().authStateChanges,
-      ),
-    ],
-    child:MaterialApp(
-      home: AuthenticationWrapper(),
+    return MaterialApp(
       initialRoute: 'welcome',
-      routes:{
-    'user':(context) => MemberInfo(),
-    'gallery':(context) => gallery(),
-    'login':(context) => login(),
-    'welcome':(context) => WelcomeScreen(),
-    'record':(context) => Records(),
-    'leaderboard':(context) => Leaderboard(),
-    'achievement':(context) => Achievement(),
-    'homescreen': (context) => HomeScreen(),
-    'memberlist' : (context)=> Members(),
-    'alumni':(context)=> Alumni(),
+      routes: {
+        //Add all the page navigation references here to navigate across screens.
+        'user':(context) => MemberInfo(),
+        'gallery':(context) => gallery(),
+        'login':(context) => login(),
+        'welcome':(context) => WelcomeScreen(),
+        'record':(context) => Records(),
+        'leaderboard':(context) => Leaderboard(),
+        'achievement':(context) => Achievement(),
+        'homescreen': (context) => HomeScreen(),
+        'memberlist' : (context)=> Members(),
+        'alumni':(context)=> Alumni(),
 
 
-    },
-    ),
+      },
     );
   }
 }
-
-class AuthenticationWrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final firebaseUser=context.watch<User>();
-    if(firebaseUser!=null){
-      return HomeScreen();
-    }
-    return login();
-
-  }
-}
-
-
-
